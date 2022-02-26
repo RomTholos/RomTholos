f8b2c4826f9afc3da889ae06bb153f0ed45d05b9
Kevin Bortis <kevin@bortis.ch>
2022-02-26 15:09:16 +0100

    POC: MGL loader for MiSTer FPGA.
    This remotly starts a game using the new MiSTer mgl (game loader) functionality.

---
+++ launcher-mister.py
@@ -0,0 +1,36 @@
+import subprocess
+import ftplib
+from pathlib import Path, PurePosixPath
+from sys import stdout
+
+MISTER_IP = "192.168.1.10"
+MISTER_USER = 'root'
+MISTER_PASSWORD = '1'
+USE_PASSWORD = True
+TARGET_GAME_FOLDER = PurePosixPath("/media/fat/games/__launcher")
+
+def play_rom(src_path):
+    #plink -batch -l root -pw 1 10.23.0.61 'echo "load_core /media/fat/_mgl/Sonic the Hedgehog 2.mgl" > /dev/MiSTer_cmd'
+    cmd="echo \"load_core /media/fat/_mgl/Sonic the Hedgehog 2.mgl\" > /dev/MiSTer_cmd"
+    #result = subprocess.run(['ssh', f'{MISTER_USER}@{MISTER_IP}', cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if USE_PASSWORD == True:
+        print('Login with password. Use private/public key auth if possible!')
+        result = subprocess.run(['plink', '-batch', '-l', MISTER_USER, '-pw', MISTER_PASSWORD, MISTER_IP , cmd], 
+            shell=True, 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE)
+    else:
+        print('Login using private/public key. Load key with pageant.')
+        result = subprocess.run(['plink', '-batch', '-l', MISTER_USER, '-pw', MISTER_PASSWORD, MISTER_IP , cmd], 
+            shell=True, 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE)
+    #print(f'ssh {user}@{host} {cmd}')
+    print(result.stdout[:-1].decode("utf-8"))
+    print(result.stderr[:-1].decode("utf-8"))
+    
+def main():
+    play_rom('./ingest/Sonic the Hedgehog 2 (World) (Rev A).md')
+
+if __name__ == '__main__':
+    main()
