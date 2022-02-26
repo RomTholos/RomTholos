bfcc87b574e77bd859a83f6d11c07e19a28cbf58
Kevin Bortis <kevin@bortis.ch>
2022-02-26 15:10:02 +0100

    Update launcher-mister.py

--- launcher-mister.py
+++ launcher-mister.py
@@ -12,7 +12,7 @@ TARGET_GAME_FOLDER = PurePosixPath("/media/fat/games/__launcher")
 def play_rom(src_path):
     #plink -batch -l root -pw 1 10.23.0.61 'echo "load_core /media/fat/_mgl/Sonic the Hedgehog 2.mgl" > /dev/MiSTer_cmd'
     cmd="echo \"load_core /media/fat/_mgl/Sonic the Hedgehog 2.mgl\" > /dev/MiSTer_cmd"
-    #result = subprocess.run(['ssh', f'{MISTER_USER}@{MISTER_IP}', cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
     if USE_PASSWORD == True:
         print('Login with password. Use private/public key auth if possible!')
         result = subprocess.run(['plink', '-batch', '-l', MISTER_USER, '-pw', MISTER_PASSWORD, MISTER_IP , cmd], 
@@ -25,9 +25,6 @@ def play_rom(src_path):
             shell=True, 
             stdout=subprocess.PIPE, 
             stderr=subprocess.PIPE)
-    #print(f'ssh {user}@{host} {cmd}')
-    print(result.stdout[:-1].decode("utf-8"))
-    print(result.stderr[:-1].decode("utf-8"))
     
 def main():
     play_rom('./ingest/Sonic the Hedgehog 2 (World) (Rev A).md')
