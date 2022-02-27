de027fc63b446ab24d3365fb96afee1bfe576f5c
Kevin Bortis <kevin@bortis.ch>
2022-02-27 10:01:44 +0100

    Added remote "file exists" check

--- launcher-mister.py
+++ launcher-mister.py
@@ -8,6 +8,7 @@ MISTER_PASSWORD = ""
 USE_PASSWORD = True
 TARGET_GAME_FOLDER = "_LaunchBox"
 TARGET_ROM_SUB = "_roms"
+FORCE_OVERWRITE = False
 
 def parse_arguments():
     ### Parsing command line arguments
@@ -23,8 +24,18 @@ def parse_arguments():
         required = True,
         type=Path,
         help = 'Path to ROM')
+    
+    parser.add_argument('--force-overwrite',
+        required = False,
+        action="store_true",
+        help = 'Force Owerwrite')
 
     args = parser.parse_args()
+
+    global FORCE_OVERWRITE
+    if args.force_overwrite == True:
+        FORCE_OVERWRITE = True
+
     return args
 
 def load_config():
@@ -65,14 +76,40 @@ def ssh_cmd(cmd):
             stderr=subprocess.PIPE)
 
 def ssh_scp(src,target):
+    exists = False
+    if not FORCE_OVERWRITE:
+        exists = ssh_test_path(target)
+            
+    if exists:
+        print("SCP: File already exists on target, skipping. Use force flag to overwrite.")
+
     # TODO: Add pub/priv key auth
-    if True:
+    if True and not exists:
         result = subprocess.run(['pscp', '-l', MISTER_USER, '-pw', MISTER_PASSWORD, src, f'{MISTER_IP}:{target}'], 
             shell=True, 
             stdout=subprocess.PIPE, 
             stderr=subprocess.PIPE)
 
-    print(f'SCP: Successfully copied {str(src)} to MiSTer.')
+        print(f'SCP: Successfully copied {str(src)} to MiSTer.')
+
+def ssh_test_path(path):
+    # ssh host "test -e /path/to/file"
+    # if [ $? -eq 0 ]; then
+    # # your file exists
+    # fi
+    #cmd="echo \"load_core /media/fat/" + f'{TARGET_GAME_FOLDER}' + "/" + f'{mgl}' + "\" > /dev/MiSTer_cmd"
+    cmd = f'test -e {path} && echo True || echo False'
+    # TODO: Add pub/priv key auth
+    if True:
+        result = subprocess.run(['plink', '-batch', '-l', MISTER_USER, '-pw', MISTER_PASSWORD, MISTER_IP , cmd], 
+            shell=True, 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE)  
+
+    if  result.stdout[:-1].decode("utf-8") == "True":
+        return True
+    else:
+        return False
 
 def write_mgl(platform, rom_path):
     # <mistergamedescription>
@@ -147,15 +184,18 @@ def launch_rom(platform, rom_path):
     play_rom(platform, rom_path, mgl.name)
     clean_up(mgl)
 
+    ssh_test_path('/media/fat/_LaunchBox/test')
+
     
 def main():
     args = parse_arguments()
+
     load_config()
     print(f'MiSTer IP address: {MISTER_IP}')
 
     launch_rom(args.p, args.r)
 
-    input("Press enter to exit")
+    #input("Press enter to exit")
 
 if __name__ == '__main__':
     main()
